69890b2261ce87e5adb2c44f60afa2e0e478dac4
echoyoung46 <vickyyoung0406@yeah.net>
2017-09-13 05:18:31 -0700

    update

--- js/common.js
+++ js/common.js
@@ -1,11 +1,32 @@
-(function (win,doc){
-  if (!win.addEventListener) return;
-  var html=document.documentElement;
-  function setFont()
-  {
-      var cliWidth=html.clientWidth;
-      html.style.fontSize=100*(cliWidth/750)+'px';
+(function(){
+  var $modules = $('[module]');
+
+  $modules.each(function(i, el) {
+    var $el = $(el),
+        modules = $el.attr('module').split(',');
+    
+    if( modules.indexOf('tab') > -1 ) {
+      initTab( $el );
+    }
+  });
+
+  function initTab( _el ) {
+    var newTab = new mo.Slide({
+      target: _el.find('.mocha-tab-panel'),
+      controller: _el.find('.mocha-tab-triggers-ul li'),
+      direction: 'x',
+      effect: 'slide',
+      circle: true,
+      autoPlay: false,
+      event: {
+        'init': function() {
+          setTimeout(function(){
+            var $panel = $(_el).find('.mocha-tab-panel')[0];
+            var _height = $($panel).height()+parseInt($($panel).css('padding-top'))+parseInt($($panel).css('padding-bottom'));
+            _el.children('.mocha-tab-panels').height( _height );
+          },0);
+        }
+      }
+    });
   }
-  win.addEventListener('resize',setFont,false);
-  setFont();
-})(window,document);
+})()
